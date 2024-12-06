124d0c935eba1210969e7824fd40817d64de21b0
Afzal Khan Jadoon <jadoonafzal027@gmail.com>
2024-12-06 09:11:28 +0500

    New Components

---
+++ src/components/Hero.js
@@ -0,0 +1,149 @@
+import React from "react";
+import profilepic3 from '../Assets/profpic3.png';
+import { TypeAnimation } from "react-type-animation";
+import ShinyEffect from "./ShinyEffect";
+import {
+  AiOutlineGithub,
+  AiOutlineInstagram,
+  AiOutlineLinkedin,
+} from "react-icons/ai";
+import {
+  DiCss3,
+  DiHtml5,
+  DiJavascript1,
+  DiNodejsSmall,
+  DiReact,
+} from "react-icons/di";
+import { motion } from "framer-motion";
+import cv from '../Assets/HuzaifaCV.pdf' // Add the path to your CV file
+
+const Hero = () => {
+  return (
+    <div className="mt-36 max-w-[1200px] mx-auto relative">
+      <div className="grid md:grid-cols-2 place-items-center gap-8">
+        <motion.div
+          initial={{ opacity: 0, y: -50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+        >
+          <TypeAnimation
+            sequence={[
+              "Front End Developer",
+              1000,
+              "UI&UX Designer",
+              1000,
+            ]}
+            speed={50}
+            repeat={Infinity}
+            className=" text-gray-400 text-xl md:text-5xl  mb-4 font-serif"
+          />
+
+          <motion.p
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 0.5 }}
+            className="text-gray-200 md:text-5xl text-3xl tracking-tight mb-4 font-serif"
+          >
+            I AM <br />
+            <span className="text-purple-500">HUZAIFA YOUSAFZAI</span>
+          </motion.p>
+
+          <motion.p
+            initial={{ opacity: 0 }}
+            whileInView={{ opacity: 1 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 1 }}
+            className="text-gray-300 max-w-[300px] md:max-w-[500px] md:text-2xl text-lg mb-6 font-serif"
+          >
+            I am a passionate Front-End Developer with over 1 year of experience.
+          </motion.p>
+
+          <motion.div
+            initial={{ opacity: 0, y: 50 }}
+            whileInView={{ opacity: 1, y: 0 }}
+            viewport={{ once: true }}
+            transition={{ duration: 1, delay: 1.5 }}
+            className="flex flex-row items-center gap-6 my-4 md:mb-0"
+          >
+            <motion.a
+              href={cv} // Link to the CV file
+              download="Huzaifa_Yousafzai_CV.pdf" // Specify the downloaded file name
+              whileHover={{
+                scale: 1.05,
+                boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.3)",
+              }}
+              className="z-10 cursor-pointer font-serif text-gray-200 md:w-auto p-4 border border-purple-400 rounded-xl"
+            >
+              Download CV
+            </motion.a>
+
+            <div className="flex gap-6 flex-row text-4xl md:text-6xl text-purple-400 z-20">
+              <motion.a
+                whileHover={{ scale: 1.2 }}
+                href="https://github.com/huzaifa898"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiOutlineGithub />
+              </motion.a>
+
+              <motion.a
+                whileHover={{ scale: 1.2 }}
+                href="https://www.linkedin.com/in/huzaifa-yousafzai-827bab236/"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiOutlineLinkedin />
+              </motion.a>
+
+              <motion.a
+                whileHover={{ scale: 1.2 }}
+                href="https://instagram.com/your-instagram-i.huzaifo5"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <AiOutlineInstagram />
+              </motion.a>
+            </div>
+          </motion.div>
+        </motion.div>
+
+        <motion.img
+          src={profilepic3}
+          className="w-[300px] md:w-[450px] object-cover border-none"
+          initial={{ opacity: 0, scale: 0.8 }}
+          whileInView={{ opacity: 1, scale: 1 }}
+          viewport={{ once: true }}
+          transition={{ duration: 1 }}
+          style={{
+            filter: "brightness(0.9) contrast(0.85)",
+            borderRadius: "10px",  // Adding some subtle rounding to the corners
+          }}
+        />
+      </div>
+
+      <motion.div
+        initial={{ opacity: 0 }}
+        whileInView={{ opacity: 1 }}
+        viewport={{ once: true }}
+        transition={{ duration: 1, delay: 2 }}
+        className="flex flex-row text-7xl px-12 md:px-0 w-full justify-center items-center py-24"
+      >
+        <p className="text-gray-200 mr-6 font-serif">My Tech Stack</p>
+        <DiHtml5 className="text-orange-600 mx-2" />
+        <DiCss3 className="text-blue-600 mx-2" />
+        <DiJavascript1 className="text-yellow-500 mx-2" />
+        <DiReact className="text-blue-500 mx-2" />
+        <DiNodejsSmall className="text-green-500 mx-2" />
+      </motion.div>
+
+      <div className="absolute inset-0 hidden md:block">
+        <ShinyEffect left={0} top={0} size={1400} />
+      </div>
+    </div>
+  );
+};
+
+export default Hero;
---
+++ src/components/Portfolio.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import project8 from '../Assets/project8.PNG';
+import project9 from "../Assets/project9.PNG";
+import project10 from "../Assets/project10.PNG";
+import project12 from "../Assets/project12.PNG";
+import project13 from "../Assets/project13.PNG";
+import project14 from  "../Assets/project14.PNG";
+
+import { AiOutlineGithub } from 'react-icons/ai';
+import Reveal from './Reveal';
+
+const projects = [
+    {
+        img: project8,
+        title: "Project #1",
+        description: "This is my final year project Investor & Enterprenur Recommender  System",
+
+        links: {
+            site: "https://huzaifa898.github.io/fyp",
+            github: "https://github.com/huzaifa898/fyp",
+        },
+    },
+    {
+        img: project9,
+        title: "Project #2",
+        description: "A fullstack application for Jobless people their they find their jobs",
+        links: {
+            github: "https://github.com/huzaifa898/jobs-seeker",
+        },
+    },
+    {
+        img: project10,
+        title: "Project #3",
+        description: "A Client Project For a Company related to Business industry and constructions",
+        links: {
+            github: "https://github.com/huzaifa898/client-pro1",
+        },
+    },
+    {
+        img: project12,
+        title: "Project #4",
+        description: "An application for biscuits agency who sell biscuits online",
+        links: {
+            github: "https://github.com/huzaifa898/client-pro2",
+        },
+    },
+    {
+        img: project13,
+        title: "Project #5",
+        description: "A project for a freelancer friend who is a web developer",
+        links: {
+            github: "https://github.com/huzaifa898/aness",
+        },
+    },
+    {
+        img: project14,
+        title: "Project #6",
+        description: "Another project for a freelancer friend who is a web developer",
+        links: {
+            github: "https://github.com/huzaifa898/my-task",
+        },
+    },
+];
+
+const Portfolio = () => {
+    return (
+        <div className='max-w-[1200px] mx-auto px-4 md:px-6 md:my-20' id="portfolio">
+            <h2 className='text-3xl font-serif text-gray-200 mb-8 text-center'>Projects</h2>
+
+            <div className='grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 justify-items-center font-serif'>
+                {projects.map((project, index) => (
+                    <Reveal key={index}>
+                        <div className='bg-gray-800 p-4 rounded-lg shadow-lg w-full max-w-[350px] flex flex-col'>
+                            <div className='mb-4'>
+                                <img
+                                    src={project.img}
+                                    alt={project.title}
+                                    className='w-full h-48 object-cover rounded-lg shadow-lg'
+                                />
+                            </div>
+                            <div className='flex flex-col justify-between flex-grow'>
+                                <h3 className='text-2xl font-semibold text-gray-200 mb-2'>{project.title}</h3>
+                                <p className='text-gray-300 mb-4'>{project.description}</p>
+                                <div className='flex space-x-4 mt-auto'>
+                                    <a href={project.links.github}
+                                        className='px-4 py-2 bg-slate-600 text-gray-200 rounded-lg hover:bg-slate-700 transition duration-300'>
+                                        <AiOutlineGithub size={20} />
+                                    </a>
+                                </div>
+                            </div>
+                        </div>
+                    </Reveal>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default Portfolio;
